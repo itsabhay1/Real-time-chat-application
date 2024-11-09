c436628121c30482fe0e02bf014075700a6046b5
itsabhay1 <abhayag41@gmail.com>
2024-11-09 20:41:50 +0530

    register login logout googleauth created

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,13 @@
+class ApiResponse{
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+// client error responses lie between 400-499
+
+
+export {ApiResponse}
